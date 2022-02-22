0cedac9848e3a707246dd8e1929453334a6140bc
pawel <pablodd@wp.pl>
2022-02-22 16:19:33 +0100

    Initial Kivy App

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Project description:
-   Simple project in python with Kivy
+Project description: 
+### Simple project in python with Kivy
 
+Python Packages:
+ - https://kivy.org/ (Version 2.0.0)
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+kivy.require('2.0.0')   # 1.9.0
 
 
-# Press the green button in the gutter to run the script.
+class KiviInfo(App):
+
+    def build(self):
+        return Label(text=self.name)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm Kivi projct (2022-02-22)')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    kiviInfo = KiviInfo()
+    kiviInfo.run()

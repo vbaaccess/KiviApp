ea694d9471eddbf46c17dcb077859c4877e9fd10
koter at home <42820377+vbaaccess@users.noreply.github.com>
2022-02-23 12:32:19 +0100

    An example of using the kv file

--- main.py
+++ main.py
@@ -3,14 +3,26 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 
+import random
+
 kivy.require('2.0.0')   # 1.9.0
+
+
+class MyRoot(BoxLayout):
+
+    def __init__(self):
+        super(MyRoot, self).__init__()
+
+    def generate_number(self):
+        pass
+        self.random_label.text = str(random.randint(1, 6))
 
 
 class KiviInfo(App):
 
     def build(self):
         # return Label(text=self.name)
-        return BoxLayout()
+        return MyRoot()
 
 
 if __name__ == '__main__':

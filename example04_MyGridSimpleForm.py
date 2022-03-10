582098f7a3bb045cbaf40aa72a07fae4cc734cd8
koter at home <42820377+vbaaccess@users.noreply.github.com>
2022-03-10 02:32:25 +0100

    Removal of UI generating code.

--- example04_MyGridSimpleForm.py
+++ example04_MyGridSimpleForm.py
@@ -1,9 +1,4 @@
-import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
